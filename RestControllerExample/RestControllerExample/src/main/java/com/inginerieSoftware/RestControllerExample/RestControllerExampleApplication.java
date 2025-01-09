2ae3cbb17fe500a5d336dd0067b53ffe4e750d97
AlexandruGH <alexandru.ghiurutan@yahoo.com>
2025-01-09 16:03:23 +0200

    Added Rest API CRUD in Spring Boot

---
+++ RestControllerExample/RestControllerExample/src/main/java/com/inginerieSoftware/RestControllerExample/RestControllerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.inginerieSoftware.RestControllerExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestControllerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestControllerExampleApplication.class, args);
+	}
+
+}
